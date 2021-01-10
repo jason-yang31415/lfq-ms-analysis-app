9eb6f8f468e4bf86d9b6bedce0f3ba3e462a1596
Jason Yang <jason.yang31415@gmail.com>
2021-01-09 19:17:06 -0500

    add pre- and post-imputation boxplot

--- src/Figures.js
+++ src/Figures.js
@@ -1,8 +1,10 @@
 import worker from "./AnalysisWorker";
 import MSExperiment from "./analysis/MSExperiment";
+import FigureSampleConditionSelector from "./components/FigureSampleConditionSelector";
 
 export const FIGURES = {
     PRE_POST_IMPUTATION_VIOLIN: "PRE_POST_IMPUTATION_VIOLIN",
+    PRE_POST_IMPUTATION_BOXPLOT: "PRE_POST_IMPUTATION_BOXPLOT",
 };
 
 export async function makePlotlyDataLayout(options) {
@@ -12,6 +14,9 @@ export async function makePlotlyDataLayout(options) {
     switch (type) {
         case FIGURES.PRE_POST_IMPUTATION_VIOLIN:
             ret = await makePrePostImputationViolin(options);
+            break;
+        case FIGURES.PRE_POST_IMPUTATION_BOXPLOT:
+            ret = await makePrePostImputationBoxplot(options);
             break;
     }
     ret.layout.autosize = true;
@@ -23,7 +28,7 @@ async function makePrePostImputationViolin({ samples, conditions }) {
     const makeViolinTrace = (trace) => {
         return Object.assign(trace, {
             type: "violin",
-            width: 3,
+            width: 1,
             points: false,
         });
     };
@@ -126,6 +131,114 @@ async function makePrePostImputationViolin({ samples, conditions }) {
         yaxis: {
             automargin: true,
         },
+    });
+    return ret;
+}
+
+async function makePrePostImputationBoxplot({ samples, conditions }) {
+    const makeBoxplotTrace = (trace) => {
+        return Object.assign(trace, {
+            type: "box",
+        });
+    };
+
+    let ret = { data: [], layout: {} };
+    if (samples != undefined && conditions == undefined) {
+        ret = {
+            data: await Promise.all([
+                ...samples.map((sample) =>
+                    worker
+                        .getData(
+                            `LFQ intensity ${sample}`,
+                            MSExperiment.SNAPSHOT_KEYS.LOG_TRANSFORM
+                        )
+                        .then((data) =>
+                            makeBoxplotTrace({
+                                name: "pre",
+                                y: data,
+                                x0: sample,
+                                legendgroup: "pre",
+                            })
+                        )
+                ),
+                ...samples.map((sample) =>
+                    worker
+                        .getData(
+                            `LFQ intensity ${sample}`,
+                            MSExperiment.SNAPSHOT_KEYS.IMPUTE_MISSING_VALUES
+                        )
+                        .then((data) =>
+                            makeBoxplotTrace({
+                                name: "post",
+                                y: data,
+                                x0: sample,
+                                legendgroup: "post",
+                            })
+                        )
+                ),
+            ]),
+            layout: {},
+        };
+    } else if (samples == undefined && conditions != undefined) {
+        const replicates = await worker.getReplicates();
+        ret = {
+            data: await Promise.all([
+                ...conditions.map((condition) =>
+                    Promise.all(
+                        replicates
+                            .get(condition)
+                            .map((sample) =>
+                                worker.getData(
+                                    `LFQ intensity ${sample}`,
+                                    MSExperiment.SNAPSHOT_KEYS.LOG_TRANSFORM
+                                )
+                            )
+                    )
+                        .then((array) => array.flat())
+                        .then((data) =>
+                            makeBoxplotTrace({
+                                name: "pre",
+                                y: data,
+                                x0: condition,
+                                legendgroup: "pre",
+                            })
+                        )
+                ),
+                ...conditions.map((condition) =>
+                    Promise.all(
+                        replicates
+                            .get(condition)
+                            .map((sample) =>
+                                worker.getData(
+                                    `LFQ intensity ${sample}`,
+                                    MSExperiment.SNAPSHOT_KEYS
+                                        .IMPUTE_MISSING_VALUES
+                                )
+                            )
+                    )
+                        .then((array) => array.flat())
+                        .then((data) =>
+                            makeBoxplotTrace({
+                                name: "post",
+                                y: data,
+                                x0: condition,
+                                legendgroup: "post",
+                            })
+                        )
+                ),
+            ]),
+            layout: {},
+        };
+    }
+
+    Object.assign(ret.layout, {
+        title: "log2 intensities pre- and post-imputation",
+        xaxis: {
+            automargin: true,
+        },
+        boxmode: "group",
+        boxgap: -1,
+        boxgroupgap: 0,
     });
     return ret;
 }
